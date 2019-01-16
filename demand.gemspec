d9fb75f68ef9fa0231f25f3b3777168b8ffb7427
Virgil Ierubino <development@convincible.media>
2019-01-16 15:57:15 +0000

    Removed encompassing module

--- demand.gemspec
+++ demand.gemspec
@@ -22,7 +22,7 @@ Gem::Specification.new do |spec|
     spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
     spec.require_paths = ['lib']
 
-    spec.add_dependency 'active_support'
+    spec.add_dependency 'activesupport'
     spec.add_dependency 'boolean'
 
     spec.add_development_dependency 'pry', '~> 0.12'
--- spec/demand_spec.rb
+++ spec/demand_spec.rb
@@ -11,159 +11,185 @@ RSpec.describe Demand do
     a_array = [1, 2, 3].freeze
     a_hash = {a: 1, b: 2, c: 3}.freeze
 
+    # Top level
+
+    it "defined demand() at the top level" do
+        expect(defined?(demand)).not_to eq(nil)
+    end
+
+    it "defined the Demand module" do
+        expect(defined?(Demand)).not_to eq(nil)
+    end
+
+    # Switches
+
+    it "lets you read options switches for how the function works" do
+        expect(Demand::YIELD_DEFAULT).not_to eq(true)
+        expect(Demand::RETURN_YIELD).not_to eq(true)
+    end
+
+    it "lets you set options switches for how the function works" do
+        Demand::YIELD_DEFAULT = true
+        Demand::RETURN_YIELD = true
+        expect(Demand::YIELD_DEFAULT).to eq(true)
+        expect(Demand::RETURN_YIELD).to eq(true)
+    end
+
     # Basic fallbacks
 
     it "returns nil for a variable that = nil" do
-        expect(Demand::demand(a_nil)).to eq(nil)
+        expect(demand(a_nil)).to eq(nil)
     end
 
     it "returns default instead if provided" do
-        expect(Demand::demand(a_nil, 'default')).to eq('default')
+        expect(demand(a_nil, 'default')).to eq('default')
     end
 
     it "returns default for empty array" do
-        expect(Demand::demand([], 'default')).to eq('default')
+        expect(demand([], 'default')).to eq('default')
     end
 
     it "returns default for empty hash" do
-        expect(Demand::demand({}, 'default')).to eq('default')
+        expect(demand({}, 'default')).to eq('default')
     end
 
     it "returns default for empty string" do
-        expect(Demand::demand("", 'default')).to eq('default')
+        expect(demand("", 'default')).to eq('default')
     end
 
     it "returns default for whitespace string" do
-        expect(Demand::demand(" \n ", 'default')).to eq('default')
+        expect(demand(" \n ", 'default')).to eq('default')
     end
 
     # Basic retrieval
 
     it "gets a variable that = false" do
-        expect(Demand::demand(a_false, 'test')).to eq(a_false)
+        expect(demand(a_false, 'test')).to eq(a_false)
     end
 
     it "gets a variable that = true" do
-        expect(Demand::demand(a_true, 'test')).to eq(a_true)
+        expect(demand(a_true, 'test')).to eq(a_true)
     end
 
     it "gets a string" do
-        expect(Demand::demand(a_string, 'default')).to eq(a_string)
+        expect(demand(a_string, 'default')).to eq(a_string)
     end
 
     it "gets a number" do
-        expect(Demand::demand(a_number, 'default')).to eq(a_number)
+        expect(demand(a_number, 'default')).to eq(a_number)
     end
 
     it "gets a array" do
-        expect(Demand::demand(a_array, 'default')).to eq(a_array)
+        expect(demand(a_array, 'default')).to eq(a_array)
     end
 
     it "gets a hash" do
-        expect(Demand::demand(a_hash, 'default')).to eq(a_hash)
+        expect(demand(a_hash, 'default')).to eq(a_hash)
     end
 
     # Typed fallbacks
 
     it "returns default for number when expecting Boolean" do
-        expect(Demand::demand(a_number, 'default', Boolean)).to eq('default')
+        expect(demand(a_number, 'default', Boolean)).to eq('default')
     end
 
     it "returns default for true when expecting FalseClass" do
-        expect(Demand::demand(a_true, 'default', FalseClass)).to eq('default')
+        expect(demand(a_true, 'default', FalseClass)).to eq('default')
     end
 
     it "returns default for false when expecting TrueClass" do
-        expect(Demand::demand(a_false, 'default', TrueClass)).to eq('default')
+        expect(demand(a_false, 'default', TrueClass)).to eq('default')
     end
 
     it "returns default for string when expecting Numeric" do
-        expect(Demand::demand(a_string, 'default', Numeric)).to eq('default')
+        expect(demand(a_string, 'default', Numeric)).to eq('default')
     end
 
     it "returns default for 0.5 when expecting Integer" do
-        expect(Demand::demand(0.5, 'default', Integer)).to eq('default')
+        expect(demand(0.5, 'default', Integer)).to eq('default')
     end
 
     it "returns default for number when expecting String" do
-        expect(Demand::demand(a_number, 'default', String)).to eq('default')
+        expect(demand(a_number, 'default', String)).to eq('default')
     end
 
     it "returns default for array when expecting Hash" do
-        expect(Demand::demand(a_array, 'default', Hash)).to eq('default')
+        expect(demand(a_array, 'default', Hash)).to eq('default')
     end
 
     it "returns default for hash when expecting Array" do
-        expect(Demand::demand(a_hash, 'default', Array)).to eq('default')
+        expect(demand(a_hash, 'default', Array)).to eq('default')
     end
 
     # Typed retrieval
 
     it "gets true when expecting Boolean" do
-        expect(Demand::demand(a_true, 'default', Boolean)).to eq(a_true)
+        expect(demand(a_true, 'default', Boolean)).to eq(a_true)
     end
 
     it "gets false when expecting Boolean" do
-        expect(Demand::demand(a_false, 'default', Boolean)).to eq(a_false)
+        expect(demand(a_false, 'default', Boolean)).to eq(a_false)
     end
 
     it "gets true when expecting TrueClass" do
-        expect(Demand::demand(a_true, 'default', TrueClass)).to eq(a_true)
+        expect(demand(a_true, 'default', TrueClass)).to eq(a_true)
     end
 
     it "gets false when expecting FalseClass" do
-        expect(Demand::demand(a_false, 'default', FalseClass)).to eq(a_false)
+        expect(demand(a_false, 'default', FalseClass)).to eq(a_false)
     end
 
     it "gets string when expecting String" do
-        expect(Demand::demand(a_string, 'default', String)).to eq(a_string)
+        expect(demand(a_string, 'default', String)).to eq(a_string)
     end
 
     it "gets number when expecting Numeric" do
-        expect(Demand::demand(a_number, 'default', Numeric)).to eq(a_number)
+        expect(demand(a_number, 'default', Numeric)).to eq(a_number)
     end
 
     it "gets 0.5 when expecting Numeric" do
-        expect(Demand::demand(0.5, 'default', Numeric)).to eq(0.5)
+        expect(demand(0.5, 'default', Numeric)).to eq(0.5)
     end
 
     it "gets 0 when expecting Integer" do
-        expect(Demand::demand(0, 'default', Integer)).to eq(0)
+        expect(demand(0, 'default', Integer)).to eq(0)
     end
 
     it "gets (3.0 / 2.0) = 1.5 when expecting Float" do
-        expect(Demand::demand(3.0 / 2.0, 'default', Float)).to eq(1.5)
+        expect(demand(3.0 / 2.0, 'default', Float)).to eq(1.5)
     end
 
     it "gets 1 when expecting Numeric" do
-        expect(Demand::demand(1, 'default', Numeric)).to eq(1)
+        expect(demand(1, 'default', Numeric)).to eq(1)
     end
 
     it "gets array when expecting Array" do
-        expect(Demand::demand(a_array, 'default', Array)).to eq(a_array)
+        expect(demand(a_array, 'default', Array)).to eq(a_array)
     end
 
     it "gets hash when expecting Hash" do
-        expect(Demand::demand(a_hash, 'default', Hash)).to eq(a_hash)
+        expect(demand(a_hash, 'default', Hash)).to eq(a_hash)
     end
 
     # Yielding
 
-    it "yields string when expecting String and returns block result" do
-        expect(Demand::demand(a_string, 'default', String) {|s| s * 2}).to eq(a_string * 2)
+    it "yields string when expecting String and returns variable, not block result" do
+        x = 'test'
+        expect(demand(a_string, 'default', String) {|s| x += s}).to eq(a_string)
+        expect(x).to eq('test' + a_string)
     end
 
     # Use cases
 
     it "replaces `if !a.nil? then x = a end` when a is a String" do
         x = ""
-        Demand::demand(a_string) {|a| x = a }
+        demand(a_string) {|a| x = a }
         expect(x).to eq(a_string)
     end
 
     it "replaces `if !a.nil? then x = a end` when a is nil" do
         x = "".freeze
-        Demand::demand(a_nil) {|a| x = a }
+        demand(a_nil) {|a| x = a }
         expect(x).to eq("")
     end
 
